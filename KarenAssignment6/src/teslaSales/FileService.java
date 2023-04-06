c84cf706c40e79ae08552069e1a7f142f02ac21f
Karen Dillehay <117693236+Kadillehay@users.noreply.github.com>
2023-04-06 08:53:26 -0500

    Add files via upload

--- KarenAssignment6/src/teslaSales/FileService.java
+++ KarenAssignment6/src/teslaSales/FileService.java
@@ -3,51 +3,39 @@ package teslaSales;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.Month;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.time.YearMonth;
-import java.util.Map;
-import java.util.stream.Collectors;
+
 
 
 public class FileService {
 
 	public List<Cars> readCars(String filename, String model) throws IOException {
-		List<Cars> salesDataList = new ArrayList<>();
+	    List<Cars> salesDataList = new ArrayList<>();
 
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MMM");
+	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM-yy", Locale.US);
 
-		Files.lines(Paths.get(filename)).skip(1).map(line -> line.split(",")).map(parts -> {
-			String[] dateParts = parts[0].split("-");
-			int year = Integer.parseInt("20" + dateParts[1]);
-			String monthAbb = dateParts[0];
+	    Files.lines(Paths.get(filename)).skip(1).map(line -> line.split(",")).map(parts -> {
+	        YearMonth yearMonth = YearMonth.parse(parts[0], formatter);
+	        int sales = Integer.parseInt(parts[1]);
 
-			int sales = Integer.parseInt(parts[1]);
+	        return new Cars(sales,model,yearMonth);
+	    }).forEach(salesDataList::add);
 
-			return new Cars(year, monthAbb, sales, model ,monthAbb);
-		}).forEach(salesDataList::add);
-
-		return salesDataList;
-
+	    return salesDataList;
 	}
 
 	public void salesForYear(List<Cars> salesDataList) {
 		Map<Integer, Integer> salesMap = salesDataList.stream()
 				.collect(Collectors.groupingBy(Cars::getYear, Collectors.summingInt(Cars::getSales)));
 
-//		System.out.println("Yearly Sales");
-//		System.out.println("--------------------");
 
 		salesMap.entrySet().stream().sorted(Map.Entry.comparingByKey())
 				.forEach(entry -> System.out.printf("%d->%d\n", entry.getKey(), entry.getValue()));
@@ -72,19 +60,28 @@ public class FileService {
 
 	}
 	public YearMonth worstMonth(List<Cars> salesDataList) {
-	    DateTimeFormatter monthFormat = DateTimeFormatter.ofPattern("MMM");
 	    Map<YearMonth, Integer> salesMap = salesDataList.stream()
-	        .filter(car -> car.getMonth() != null)
+	        .filter(car -> car.yearMonth() != null)
 	        .collect(Collectors.groupingBy(
-	            car -> YearMonth.of(car.getYear(), Month.parse(car.getMonth(), monthFormat)),
+	            Cars::yearMonth,
 	            Collectors.summingInt(Cars::getSales)
 	        ));
 	    Optional<Map.Entry<YearMonth, Integer>> minEntry = salesMap.entrySet().stream()
-	        .min(Comparator.comparingInt(Map.Entry::getValue));
+	        .min(Map.Entry.comparingByValue());
 	    return minEntry.map(Map.Entry::getKey).orElse(null);
 	}
+	public YearMonth bestMonth(List<Cars> salesDataList) {
+	    Map<YearMonth, Integer> salesMap = salesDataList.stream()
+	        .filter(car -> car.yearMonth() != null)
+	        .collect(Collectors.groupingBy(
+	            Cars::yearMonth,
+	            Collectors.summingInt(Cars::getSales)
+	        ));
+	    Optional<Map.Entry<YearMonth, Integer>> maxEntry = salesMap.entrySet().stream()
+	        .max(Map.Entry.comparingByValue());
+	    return maxEntry.map(Map.Entry::getKey).orElse(null);
 	}
-	
+}
 	
    
 
--- KarenAssignment6/src/teslaSales/TeslaApplication.java
+++ KarenAssignment6/src/teslaSales/TeslaApplication.java
@@ -8,18 +8,18 @@ public class TeslaApplication {
 	public static void main(String[] args) throws IOException {
 		FileService fileService = new FileService();
 
-//		 fileService.readCars("modelX.csv", "modelX");
+
 		System.out.println("Model X yearly sales report: ");
 		System.out.println("-----------------------------");
 		
-//		System.out.println("The best month for Model X was: ");
-	
+		
 		 List<Cars> salesDataListModelX= fileService.readCars("C:\\Users\\kadil\\eclipse-workspace\\KarenAssignment6\\src\\teslaSales\\modelX.csv", "modelX");
 		fileService.salesForYear(salesDataListModelX);
 		System.out.println("   ");
 		
 		System.out.println("Best sales year for Model X: " + fileService.carBestYear(salesDataListModelX));
 		System.out.println("Worst sales year for Model X: " + fileService.carWorstYear(salesDataListModelX));
+		System.out.println("Best sales Month for Model X: " + fileService.bestMonth(salesDataListModelX));
 		System.out.println("Worst sales Month for Model X: " + fileService.worstMonth(salesDataListModelX));
 		System.out.println("   ");
 		System.out.println("Model S yearly sales report: ");
@@ -31,17 +31,20 @@ public class TeslaApplication {
 			System.out.println("   ");
 			System.out.println("Best sales year for Model S: " + fileService.carBestYear(salesDataListModelS));
 			System.out.println("Worst sales year for Model S: " + fileService.carWorstYear(salesDataListModelS));
+			System.out.println("Best sales Month for Model S: " + fileService.bestMonth(salesDataListModelS));
+			System.out.println("Worst sales Month for Model S: " + fileService.worstMonth(salesDataListModelS));
 			System.out.println("   ");
 			System.out.println("Model 3 yearly sales report: ");
 			System.out.println("-----------------------------");
 			
 			
-			System.out.println( fileService.carBestYear(salesDataListModelS));
 		 List<Cars> salesDataListModel3= fileService.readCars("C:\\Users\\kadil\\eclipse-workspace\\KarenAssignment6\\src\\teslaSales\\model3.csv", "model3");
 				fileService.salesForYear(salesDataListModel3);
 				System.out.println("   ");
 				System.out.println("Best sales year for Model 3: " + fileService.carBestYear(salesDataListModel3));
 				System.out.println("Worst sales year for Model 3: " + fileService.carWorstYear(salesDataListModel3));
+				System.out.println("Best sales Month for Model 3: " + fileService.bestMonth(salesDataListModel3));
+				System.out.println("Worst sales Month for Model 3: " + fileService.worstMonth(salesDataListModel3));
 				
 				
 	}
